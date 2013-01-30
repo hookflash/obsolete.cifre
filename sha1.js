e865d8319afca033e6e76d1e3adc339d47bae04b
Tim Caswell <tim@creationix.com>
2013-01-30 12:17:28 -0600

    Add stubs for md5, rsa, sha1, and sha256

---
+++ sha1.js
@@ -0,0 +1,9 @@
+// Fast SHA1 hash using typed arrays
+// Copyright © 2013 Hookflash Inc.
+// Released under the MIT license.
+// Written by Tim Caswell <tim@creationix.com>
+
+define('sha1', function () {
+  "use strict";
+
+});
---
+++ sha256.js
@@ -0,0 +1,9 @@
+// Fast SHA256 hash using typed arrays
+// Copyright © 2013 Hookflash Inc.
+// Released under the MIT license.
+// Written by Tim Caswell <tim@creationix.com>
+
+define('sha256', function () {
+  "use strict";
+
+});
