150218dc9da5b605315237c671dfbaa35ab07f5f
Mike Pennisi <mike@mikepennisi.com>
2013-03-20 10:20:46 -0400

    Add failing test case

--- test/test-sha256.js
+++ test/test-sha256.js
@@ -10,7 +10,9 @@ var tests = [
   "message digest", "f7846f55cf23e14eebeab5b4e1550cad5b509e3348fbc4efa3a1413d393cb650",
   "abcdefghijklmnopqrstuvwxyz", "71c480df93d6ae2f1efad1447c66c9525e316218cf51fc8d9ed832f2daf18b73",
   "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
-    "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1"
+    "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1",
+  'contact:{"section":{"$id":"A","cipher":"sha256/aes256","created":1363698704,"expires":1426770704,"saltBundle":{"salt":{"$id":"c5f568521a24c5baf618d1ada3b8e300fdc963e0","#text":"432d09beaed2b7bd392a73c8c3dddf86098981bc"},"signature":{"reference":"#c5f568521a24c5baf618d1ada3b8e300fdc963e0","algorithm":"http://openpeer.org/2012/12/14/jsonsig#rsa-sha1","digestValue":"0a0f158931d9e6abe2e3d99a7841242762a86feb","digestSigned":"by5doZ5jRK12qi4lIFUdeHpYR7ta3AUesWQX0Odr9nUL9MsdJTyccLeZMXFt2dcQKtfIyzOwFkpUbQuH7IFB4JLgPnGkJW/WfEggGxisTSDr+CYi3NU0hStWDvC+m6OLXjQAOc0PeI3ketUSXcEiNukkOvxuBlflbE0Zf7/zXejG+9L6Ve/0z3eKsJ487gyjhPhGxgLoGb1G6+3jWNvBubUqVhYac3hVMvI95zIkZg44T25gnuEwKfXpkfKRZRptbuk1Dq6StA52ZBKn1xQM8z3akPj9CPLcSTW3Rb+CiG3tgyRxBl7nYBaJeGDWNxtF0B9ttBY46AV69ugrdJlRbA==","key":{"$id":"5d4e02f0800c0f354a72b2983914ca409ce6cf0c","domain":"unstable.hookflash.me","service":"salt"}}}}}',
+  "dcbe8e9a46a0ab4441e5502f9561dd1d32cad5e6c09328df0eb0822b502c0bf3"
 ]
 
 describe("sha256", function () {
