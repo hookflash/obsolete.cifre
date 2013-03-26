bd2fbc2b1d2fb1e3d35da72b0a21978c2257ebc6
Tim Caswell <tim@creationix.com>
2013-03-26 15:09:12 -0500

    Fix sha1 for larger inputs

--- sha1.js
+++ sha1.js
@@ -149,7 +149,7 @@
         var b = 0,
             o = offset + i;
         if (o < inputLength) {
-          b = input[i];
+          b = input[o];
         }
         else if (o === inputLength) {
           b = 0x80;
